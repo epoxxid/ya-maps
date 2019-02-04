be61678aaba18cd22e65e57ea1f8d23b06c85c56
Roman Kovalev <epoxxid@gmail.com>
2019-02-04 16:02:47 +0300

    Add gulpfile and required dependencies

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { dest, series } = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const rename = require('gulp-rename');
+const sourceMaps = require('gulp-sourcemaps');
+
+const config = {
+    js: {
+        inputFile: 'src/index.js',
+        outputName: 'map.js',
+        outputDir: './dist/',
+        mapsDir: './maps/'
+    }
+};
+
+function bundle(bundler) {
+    bundler.bundle()
+        .pipe(source(config.js.inputFile))
+        .pipe(buffer())
+        .pipe(rename(config.js.outputName))
+        .pipe(sourceMaps.init({loadMaps: true}))
+        .pipe(sourceMaps.write(config.js.mapsDir))
+        .pipe(dest(config.js.outputDir));
+}
+
+function build(cb) {
+
+    let bundler = browserify(config.js.inputFile)
+        .transform(babelify);
+
+    bundle(bundler);
+
+    cb();
+}
+
+
+exports.build = build;
